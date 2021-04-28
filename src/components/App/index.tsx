38d89b3111f689b7c9b86fc4264486e4b1497005
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-04-28 11:21:43 -0500

    ADAIG-9: updating styling

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,17 +1,38 @@
 import "./styles.css"
+import CheckCircleIcon from "@material-ui/icons/CheckCircle"
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked"
+import { useState } from "react"
 
 export function App() {
+  const [clicked, setClicked] = useState(false)
+
+  const handleIconClick = (): void => {
+    if (!clicked) {
+      setClicked(true)
+    } else {
+      setClicked(false)
+    }
+
+    // change <AddCircleIcon /> to <BlockIcon /> at "id"
+  }
   return (
     <div className="App">
       <div id="header">
-        <p id="header-title">Todo App</p>
+        <p id="header-title">My Tasks</p>
       </div>
 
       <div className="active">
         <div className="content">
-          <div className="checkbox">
-            <span className="ellipse"></span>
+          {/* <div className="checkbox"> */}
+          <div onClick={handleIconClick}>
+            {clicked ? (
+              <CheckCircleIcon className="ellipse" />
+            ) : (
+              <RadioButtonUncheckedIcon className="ellipse" />
+            )}
           </div>
+
+          {/* </div> */}
           <div className="text-date">
             <p className="text">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
