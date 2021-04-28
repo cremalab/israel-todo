de2d5b1ed1c49ecc808fee9972c2bdfc0cc12677
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-04-28 14:59:44 -0500

    ADAIG-9: corrected flex box and styling

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,9 @@
 import "./styles.scss"
 import CheckCircleIcon from "@material-ui/icons/CheckCircle"
+import MenuIcon from "@material-ui/icons/Menu"
 import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked"
 import { useState } from "react"
-import { NewTask } from "../NewTask"
+// import { NewTask } from "../NewTask"
 
 export function App() {
   const [clicked, setClicked] = useState(false)
@@ -19,37 +20,40 @@ export function App() {
   return (
     <div className="App">
       <div id="header">
+        <MenuIcon className="menu-icon" />
         <p id="header-title">My Tasks</p>
       </div>
-
-      <div className="active">
-        <div className="content">
-          <div onClick={handleIconClick}>
-            {clicked ? (
-              <CheckCircleIcon className="ellipse" />
-            ) : (
-              <RadioButtonUncheckedIcon className="ellipse" />
-            )}
+      <div className="wrapper">
+        <div className="active-todos">
+          <h3 id="active-tasks">Active Tasks</h3>
+          <div className="card">
+            <div className="content">
+              <div onClick={handleIconClick}>
+                {clicked ? (
+                  <CheckCircleIcon className="ellipse" />
+                ) : (
+                  <RadioButtonUncheckedIcon className="ellipse" />
+                )}
+              </div>
+              <div className="text-date">
+                <p className="text">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Pariatur repudiand
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="text-date">
-            <p className="text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
-              repudiand
-            </p>
-          </div>
+          <button
+            className="large"
+            onClick={() => {
+              console.log("clicked")
+            }}
+          >
+            <p id="btn-text">Create New Task</p>
+          </button>
         </div>
       </div>
-      <NewTask />
-      <h3 id="active-tasks">Active Tasks</h3>
-
-      <button
-        className="large"
-        onClick={() => {
-          console.log("clicked")
-        }}
-      >
-        <p id="btn-text">Create New Task</p>
-      </button>
+      {/* <NewTask /> */}
     </div>
   )
 }
