4861fc159e687c9e0bee45b8e7e43d6f8362acf0
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-05-04 18:16:51 -0500

    ADAIG-12: adding fields to missing test for TodoCard due to update on props for completing a todo

--- src/components/TodoCard/TodoCard.test.tsx
+++ src/components/TodoCard/TodoCard.test.tsx
@@ -3,15 +3,24 @@ import { TodoCard } from "."
 
 describe("TodoCard", () => {
   test("it renders a todo", () => {
+    const handleIcon = jest.fn()
     const todoText = "Create testing for components"
-    const { getByText } = render(<TodoCard todo={todoText} />)
+    const { getByText } = render(
+      <TodoCard todo={todoText} clicked={false} handleIconClick={handleIcon} />,
+    )
     const element = getByText(todoText)
     expect(element).toBeInTheDocument()
   })
   test("completed todo is striked", () => {
+    const handleIcon = jest.fn()
     const todoText = "Create testing for components"
     const { getByText } = render(
-      <TodoCard todo={todoText} isCompleted={true} />,
+      <TodoCard
+        todo={todoText}
+        isCompleted={true}
+        clicked={true}
+        handleIconClick={handleIcon}
+      />,
     )
     const element = getByText(todoText)
     expect(element).toHaveClass("striked-text")
