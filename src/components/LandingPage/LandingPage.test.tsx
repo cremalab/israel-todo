b22cc1074a2106a93fcbc1d3cc0c03702cb121dc
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-06-02 16:53:47 -0500

    ADAIG-19: adding landing page and routing

---
+++ src/components/LandingPage/LandingPage.test.tsx
@@ -0,0 +1,6 @@
+import { LandingPage } from "./LandingPage"
+import React from "react"
+
+describe("LandingPage", () => {
+  it.todo(`LandingPage needs to be tested`)
+})
---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,29 @@
+import "./styles.scss"
+import DoneIcon from "@material-ui/icons/Done"
+import { Link } from "react-router-dom"
+
+export function LandingPage() {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="container">
+          <div id="oval"></div>
+          <div id="todo-section">
+            <DoneIcon className="checkmark" style={{ fontSize: "5em" }} />
+            <div id="underline"></div>
+            <h1 id="title">todo</h1>
+          </div>
+        </div>
+      </div>
+      <div className="welcome-section">
+        <h2>Welcome!</h2>
+        <p>Get things done with Todo.</p>
+        <Link to="todo" className="link">
+          <button>
+            <span>Get Started</span>
+          </button>
+        </Link>
+      </div>
+    </>
+  )
+}
