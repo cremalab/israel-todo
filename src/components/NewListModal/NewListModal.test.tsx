0ff057d66600e108035a871833753ca2cd7d0852
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-06-09 10:57:54 -0500

    ADAIG-22: adding tests for new components, sideNav and NewListModal

--- src/components/NewListModal/NewListModal.test.tsx
+++ src/components/NewListModal/NewListModal.test.tsx
@@ -1,6 +1,28 @@
-import React from "react"
+import { fireEvent, render } from "@testing-library/react"
 import { NewListModal } from "./NewListModal"
 
 describe("NewListModal", () => {
-  it.todo(`NewListModal needs to be tested`)
+  it("captures a value when onChange", () => {
+    const list = ["List Name"]
+    const setListNames = jest.fn()
+    const setShowListModal = jest.fn()
+
+    const { getByPlaceholderText, getByText } = render(
+      <NewListModal
+        showListModal={true}
+        listNames={list}
+        setListNames={setListNames}
+        setShowListModal={setShowListModal}
+      />,
+    )
+
+    const input = getByPlaceholderText("List Name") as HTMLInputElement
+
+    fireEvent.change(input, { target: { value: "My Tasks" } })
+
+    const saveButton = getByText("Save")
+    fireEvent.click(saveButton)
+
+    expect(input.value).toBe("My Tasks")
+  })
 })
--- src/components/SideNav/SideNav.test.tsx
+++ src/components/SideNav/SideNav.test.tsx
@@ -1,6 +1,23 @@
-import React from "react"
+import { fireEvent, render } from "@testing-library/react"
 import { SideNav } from "./SideNav"
 
 describe("SideNav", () => {
-  it.todo(`SideNav needs to be tested`)
+  it("should allow user to create new task", () => {
+    const list = ["List Name"]
+    const openListModal = jest.fn()
+
+    const { getByText } = render(
+      <SideNav
+        listNames={list}
+        openListModal={openListModal}
+        showSideNav={true}
+      />,
+    )
+
+    const createButton = getByText("+ Create New List")
+
+    fireEvent.click(createButton)
+
+    expect(openListModal).toHaveBeenCalled()
+  })
 })
