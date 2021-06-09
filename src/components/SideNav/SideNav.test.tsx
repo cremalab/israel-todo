d7462611b83c325e9614606bd6449343e0316215
Israel Guillermo Cano <israelguillermo22@gmail.com>
2021-06-09 16:53:27 -0500

    ADAIG-24: adding additional test on sidebarNav

--- src/components/SideNav/SideNav.test.tsx
+++ src/components/SideNav/SideNav.test.tsx
@@ -1,9 +1,14 @@
 import { fireEvent, render } from "@testing-library/react"
+import { addList } from "../../ListStore/lists"
+import { store } from "../../store"
 import { StateProvider } from "../StateProvider"
 import { SideNav } from "./SideNav"
 
 describe("SideNav", () => {
   it("should allow user to create new task", () => {
+    store.dispatch(
+      addList({ id: "12345", name: "Things todo for the application" }),
+    )
     const openListModal = jest.fn()
 
     const { getByText } = render(
@@ -17,5 +22,7 @@ describe("SideNav", () => {
     fireEvent.click(createButton)
 
     expect(openListModal).toHaveBeenCalled()
+
+    expect(getByText("Things todo for the application")).toBeInTheDocument()
   })
 })
